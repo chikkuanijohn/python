338cabc7fe32ed737fa5df55a83af5a087f171e1
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-06-21 11:13:43 +0530

    new file

--- bonous.py
+++ bonous.py
@@ -1,5 +1,5 @@
 a=int(input("enter salary"))
-b=int(input("enter vars"))
+b=int(input("enter years"))
 if b>=5:
     total=a*0.05
     print(" bonous",total)
--- forloop.py
+++ forloop.py
@@ -1,29 +1,47 @@
-""""for i in range(10):
+"""for i in range(10):
     print(i)"""
 """for i in range(1,11):
     print(i)"""
 """for i in range(1,11,2):
     print(i)"""
-"""a=int(input("Enter starting no"))
-b=int(input("Enter ending no"))
+a=int(input("enter starting no"))
+b=int(input("enter ending no"))
+for i in range(a,b+1): 
+    print(i) 
+#
+a=int(input("enter starting no"))
+b=int(input("enter ending point"))
 sum=0
 for i in range(a,b+1):
-     sum=sum+i
-print(sum)"""""
-"""a=int(input("Enter starting no"))
+    sum=sum+i
+    print(sum)   
+#
+a=int(input("enter stating no"))
+b=int(input("enter ending no"))
+for i in range(a,b+1):
+ if i%2==0:
+print(i)
+#
+"""a=int(input("enter starting no"))
+b=int(input("enter ending no"))
+if i in range(a,b+1):
+if i%2==0:
+print(i)"""  
+a=int(input("enter starting no"))
 b=int(input("enter ending no"))
 sum=0
 for i in range(a,b+1):
-    if i%2==1:
-        sum=sum+i
-print(sum)"""
-
-a=int(input("enter number"))
-fact=1
-for i in range(1,a+1):
-    fact=fact*i
-print(fact)
-
+   if i%2==0:
+      sum=sum+i
+      print(sum)
+#
+a=int(input("enter starting no"))
+b=int(input("enter ending no")) 
+sum=0
+for i in range(a,b+1):
+   if i%2==1:
+      sum=sum+i
+      print(sum)         
 
         
         
