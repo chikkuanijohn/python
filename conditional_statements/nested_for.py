f363393209d4b286784aef2267095e8bc13e6a02
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-06-24 11:22:11 +0530

    new file

---
+++ conditional_statements/nested_for.py
@@ -0,0 +1,10 @@
+"""for i in range(4):
+    for j in range(3):
+        print(j,end="\t")
+    print()"""
+a=1
+for i in range(3):
+    for j in range(3):
+        print(a,end="\t")
+        a+=1 
+    print()                    
