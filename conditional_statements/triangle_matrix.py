3815d62b44d6398a4487d999ba927a7d8de528c2
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-06-25 11:23:56 +0530

    new file

--- conditional_statements/triangle_matrix.py
+++ conditional_statements/triangle_matrix.py
@@ -1,4 +1,86 @@
-for i in range(1,4):
+"""for i in range(1,4):
     for j in range(i):
         print(j,end="\t")
+    print()"""
+
+"""for i in range(1,4):
+    for j in range(i):
+        print(i-1,end="\t")
+    print()"""
+
+"""a=0
+for i in range(1,4):
+    for j in range(i):
+        print(a,end="\t")
+        a+=1
+    print()""" 
+
+"""for i in range(1,4):
+    a=i-1
+    for j in range(i):
+        print(a,end="\t")
+        a-=1
+    print()"""
+
+"""a=65
+for i in range(3):
+    for j in range(4):
+        print(chr(a),end="\t")
+    print()  
+    a+=1"""
+
+"""a=65
+for i in range(3):
+    for j in range(3):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
+
+"""for i in range(3):
+    a=65
+    for j in range(3):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
+
+"""a=65
+for i in range(1,4):
+    for j in range(i):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
+
+"""for i in range(1,4):
+    a=65
+    for j in range(i):
+        print(chr(a),end="\t")
+        a+=1
+    print()"""
+
+"""a=65
+for i in range(1,4):
+    for j in range(i):
+        print(chr(a),end="\t")
     print()
+    a+=1"""
+
+a=65
+for i in range(1,4):
+    for j in range(i):
+        print(chr(a-j),end="\t")
+    print()
+    a+=1    
+     
+
+    
+        
+
+
+
+    
+
+
+
+
+  
+  
