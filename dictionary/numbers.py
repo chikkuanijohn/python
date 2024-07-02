2d1098f7e55e3634148ca8a3fcc3a1bdf66305de
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-07-02 19:51:41 +0530

    new file

--- dictionary/numbers.py
+++ dictionary/numbers.py
@@ -1,6 +1,12 @@
-num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five'}
-a=int(input("enter the numbers"))
-rev=0
-for i in (num):
-    rev=i+rev
-    print(rev)
+number={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+a=int(input('enter the number:'))
+result=''
+while a>0:
+    digit = a%10
+    word=number[digit]
+    result = word+''+result
+    a=a//10
+result=result
+print(result)
+
+
