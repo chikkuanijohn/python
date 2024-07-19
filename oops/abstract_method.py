bd9a52e597dfafa590979dc45a4de409f0412c0a
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-07-19 18:30:53 +0530

    new file

---
+++ oops/abstract_method.py
@@ -0,0 +1,88 @@
+# from abc import ABC,abstractmethod
+# class animal(ABC):
+#     @abstractmethod
+#     def make_sound(s):
+#         print('sound')
+# class birds(animal):
+#     def fly(s):
+#         print('fly')
+#     def make_sound(s):
+#         print('bird sound')
+# class cat(animal):
+#     def run(s):
+#         print('run')
+#     def make_sound(s):
+#         print('cat sound') 
+# b1=birds()
+# b1.fly()
+# b1.make_sound()
+
+# print('cat')
+# c1=cat()
+# c1.run()
+
+#regular expression:
+
+# a='welcome'
+# import re
+# re.sub('w','W',a)
+# re.sub('welcome','python',a)
+# print(re.sub('welcome','python',a))
+# print(re.sub('w','W',a))
+# print(re.split('e',a))
+# print(re.split('e',a,1))
+
+# a='hello'
+# import re
+# re.sub('h','H',a)
+# print(re.sub('h','H',a))
+# re.split('e',a)
+# print(re.split('e',a))
+
+# a='welcome'
+# import re
+# re.findall('e',a)
+# print(re.findall('e',a))
+# len(re.findall('e',a))
+# print(len(re.findall('e',a)))
+
+# re.search('e',a)
+# print(re.search('e',a))
+# re.search('z',a)
+# print(re.search('z',a))
+# if re.search('z',a):
+#     print("yes")
+# else:
+#     print("no")    
+
+# if re.search('l',a):
+#     print("yes")
+# else:
+#     print("no")  
+# a='abcd'
+# import re 
+# re.search('d.',a)
+# print(re.search('d.',a))
+# re.search('b..',a)
+# print(re.search('b..',a))
+
+# re.search('d.*',a)
+# print(re.search('d.*',a))
+# re.search('d.+',a)
+# print(re.search('d.+',a))
+# re.search('d.?',a)
+# print(re.search('d.?',a))
+
+
+a='welcome'
+import re
+# print(re.search('e.'a))
+# print(re.search('w*',a))
+# print(re.search('l.+',a))
+# print(re.search('c.?',a))
+
+
+
+
+
+
--- oops/inheritance.py
+++ oops/inheritance.py
@@ -1,27 +1,59 @@
-class synnefo:
-    def __init__(self):
-        print("register")
-    def python(s):
-        print("python")
-    def php(self):
-        print("php") 
+# class synnefo:
+#     def __init__(self):
+#         print("register")
+#     def python(s):
+#         print("python")
+#     def php(self):
+#         print("php") 
 
-class novavi(synnefo):
-    def dm(self):
-        print("dm")
-    def web_dev(self):
-        print("web_dev") 
-manu=synnefo()
-manu.python()
-sanu=novavi()
-sanu.python()
+# class novavi(synnefo):
+#     def dm(self):
+#         print("dm")
+#     def web_dev(self):
+#         print("web_dev") 
+# manu=synnefo()
+# manu.python()
+# sanu=novavi()
+# sanu.python()
+
+# class synnefo:
+#     def __init__(s):
+#         print("register")
+#     def python(s):
+#         print("python") 
+#     def php(self):
+#         print("php")
+
+#     def dm(self):
+#         print("dm") 
+#     def web_dev(self):
+#         print("web_dev") 
+# class student(novavi,synnefo):
+#     def reg(self):
+#         print("reg")
+# manu=synnefo()
+# manu.python()
+# sanu=novavi()
+# sanu.web_dev()
+# anu=student()
+# anu.reg()         
+
+#multilevel inheritance:
 
 class synnefo:
     def __init__(s):
         print("register")
-    def python(s):
-        print("python") 
-    def php(self):
+           
+
+
+
+
+    
+
+
+    
+
+    
         
 
 
